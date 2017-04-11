b5a50fae10632eb2cecad35647b0737b9d46c777
Jia Ming <jiaming@picoded.com>
2017-04-11 15:09:16 +0800

    able to scrap app5 and app3 and output the results together with exploit generation

--- utils/JavascriptScrap.py
+++ utils/JavascriptScrap.py
@@ -4,6 +4,8 @@ import scrapy
 import json
 from scrapy.spider import BaseSpider
 from scrapy.selector import HtmlXPathSelector
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+from scrapy.utils.response import get_base_url
 
 class JavascriptScrap(BaseSpider):
     name = "jsSpider"
@@ -12,6 +14,7 @@ class JavascriptScrap(BaseSpider):
         "https://app1.com/",
         "https://app5.com/",
         "https://app7.com/",
+        "https://app9.com/"
     ]
     def verifyAbsolutePath(self, path):
         return bool(urlparse.urlparse(path).netloc)
@@ -30,12 +33,14 @@ class JavascriptScrap(BaseSpider):
     def extractEndPointsFromJavascript(self, url, host):
         meth = url.split(",")
         meth[1] = meth[1].strip(" \"\'\n\t\r")
-        components={}
-        if self.verifyAbsolutePath(meth[1]):
-            components = urlparse.urlparse(meth[1])
+        self.checkIfSrcHasParam(meth[1], host)
 
+    def checkIfSrcHasParam(self, url, host):
+        components={}
+        if self.verifyAbsolutePath(url):
+            components = urlparse.urlparse(url)
         else:
-            components = urlparse.urlparse(host+meth[1])
+            components = urlparse.urlparse(host+url)
         parameters = urlparse.parse_qs(components.query)
         result = {components.scheme+"://"+components.netloc+components.path:[{"type":"GET"}, parameters]}
         parsed = json.dumps(result)
@@ -45,9 +50,9 @@ class JavascriptScrap(BaseSpider):
             outfile.close()
 
     def connectToJavaScriptSrc(self, src, host):
+        requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
         r = requests.get(src, verify = False)
         bodyList = r.content.split("\r")
-        print src
         for index, content in enumerate(bodyList):
             endpoint = ""
             if "client.open" in content:
@@ -63,10 +68,12 @@ class JavascriptScrap(BaseSpider):
 
     def javascriptStart(self, response):
         jsList = response.xpath("//script/@src").extract()
-        print jsList
-        thislist = self.scrapJavascript(jsList, response.url);
+        # print jsList
+        base_url = get_base_url(response)
+        thislist = self.scrapJavascript(jsList, base_url);
         for urlSrc in thislist:
-            self.connectToJavaScriptSrc(urlSrc, response.url)
+            self.connectToJavaScriptSrc(urlSrc, base_url)
+            self.checkIfSrcHasParam(urlSrc, base_url)
 
     def parse(self, response):
         self.javascriptStart(response)
